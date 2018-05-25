55af5abd3a41ecc0d52bf83bffd1c701c5630472
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-05-25 02:02:09 -0500

    Fixing some views and deleting unnecessary files

--- src/main/java/pe/edu/upc/controller/HomeController.java
+++ src/main/java/pe/edu/upc/controller/HomeController.java
@@ -2,10 +2,12 @@ package pe.edu.upc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
-public class IndexController {
-
+@RequestMapping(value= "/")
+public class HomeController {
 	@GetMapping("/")
 	public String index() {
 		return "index";
