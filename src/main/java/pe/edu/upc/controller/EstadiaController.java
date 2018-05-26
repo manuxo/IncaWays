f60ca4c9f835b959b3f3ec1cfaafc98d060643ac
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-05-25 19:22:42 -0500

    Adding RequestMapping method to EstadiasController and views /estadia/ver/{id}

--- src/main/java/pe/edu/upc/controller/EstadiaController.java
+++ src/main/java/pe/edu/upc/controller/EstadiaController.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import pe.edu.upc.entity.Estadia;
+import pe.edu.upc.entity.Vuelo;
 import pe.edu.upc.service.IEstadiaService;
 
 @Controller
@@ -33,5 +36,19 @@ public class EstadiaController {
 		model.addAttribute("estadia",new Estadia());
 		
 		return "estadia/listar";
+	}
+	
+	
+	@GetMapping(value = "/estadia/ver/{id}")
+	public String ver(@PathVariable(value = "id") Long id, Model model, RedirectAttributes flash) {
+
+		Estadia estadia = servicio.findById(id);
+		if (estadia == null) {
+			flash.addFlashAttribute("error", "La estadia no existe en la base de datos");
+			return "redirect:estadia/listar";
+		}
+
+		model.addAttribute("estadia", estadia);
+		return "estadia/ver";
 	}
 }
