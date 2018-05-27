f8f758240e5921da5ff718657990102da30d80a8
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-05-27 12:46:44 -0500

    create vuelo fixed

--- src/main/java/pe/edu/upc/controller/VueloController.java
+++ src/main/java/pe/edu/upc/controller/VueloController.java
@@ -1,6 +1,8 @@
 package pe.edu.upc.controller;
 
 
+import java.sql.Date;
+import java.sql.Time;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -24,7 +26,6 @@ import pe.edu.upc.entity.Vuelo;
 import pe.edu.upc.service.IEmpresaVueloService;
 import pe.edu.upc.service.IUsuarioService;
 import pe.edu.upc.service.IVueloService;
-
 
 @Controller
 public class VueloController {
@@ -83,7 +84,7 @@ public class VueloController {
 		List<Empresavuelo> empresavuelos = servicioEmpresaVuelo.findAll();
 		model.addAttribute("empresavuelos", empresavuelos);
 		
-		model.addAttribute("vuelo",new Vuelo());
+		model.addAttribute("contenedor",new ContenedorFormulario());
 		model.addAttribute("titulo","Publicar Vuelo");
 		return "vuelo/crear";
 	}
@@ -91,13 +92,59 @@ public class VueloController {
 	
 	@RequestMapping(value = "/vuelo/crear", method= RequestMethod.POST)
 	// public String guardar(Compravuelo compravuelo) {
-	public String guardar(@Valid Vuelo vuelo, BindingResult bindingResult,Model model) {
-		
-		System.out.print("\n\n\n ENTREEEE \n\n\n");
-		
-		servicio.saveVuelo(vuelo);
-		
+	public String guardar(@Valid ContenedorFormulario contenedor, BindingResult bindingResult,Model model) {
+		contenedor.getVuelo().setFechasalida(formatStringToSqlDate(contenedor.getFechasalida()));
+		contenedor.getVuelo().setHorasalida(Time.valueOf(contenedor.getHorasalida()));
+		servicio.saveVuelo(contenedor.getVuelo());
 		return "redirect:/vuelo/listar";
 	}
 	
+	private Date formatStringToSqlDate(String strDate) {
+		Date fecha = null;
+		String anios = strDate.substring(0,4);
+		
+		String meses = strDate.substring(5,7);
+		
+		String dias = strDate.substring(8);
+		
+		@SuppressWarnings("deprecation")
+		java.util.Date fechaFormateada = new java.util.Date(Integer.parseInt(anios), Integer.parseInt(meses), Integer.parseInt(dias));
+		
+		
+		@SuppressWarnings("deprecation")
+		java.util.Date base = new java.util.Date(1969,12,30);
+		
+		fecha = new Date(fechaFormateada.getTime() - base.getTime());
+		return fecha;
+	}
+}
+
+
+class ContenedorFormulario{
+	private Vuelo vuelo;
+	private String fechasalida;
+	private String horasalida;
+	
+	public ContenedorFormulario() {
+		this.vuelo = new Vuelo();
+	}
+	
+	public Vuelo getVuelo() {
+		return vuelo;
+	}
+	public void setVuelo(Vuelo vuelo) {
+		this.vuelo = vuelo;
+	}
+	public String getFechasalida() {
+		return fechasalida;
+	}
+	public void setFechasalida(String fechasalida) {
+		this.fechasalida = fechasalida;
+	}
+	public String getHorasalida() {
+		return horasalida;
+	}
+	public void setHorasalida(String horasalida) {
+		this.horasalida = horasalida;
+	}
 }
