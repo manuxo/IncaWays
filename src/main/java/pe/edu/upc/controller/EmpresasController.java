cecd3c3c9cabb68c589a41c28476bf019581ec12
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-05-27 17:54:43 -0500

    create and list methods finished for all entities

--- src/main/java/pe/edu/upc/controller/EmpresasController.java
+++ src/main/java/pe/edu/upc/controller/EmpresasController.java
@@ -1,9 +1,18 @@
 package pe.edu.upc.controller;
 
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import pe.edu.upc.entity.Empresaestadia;
+import pe.edu.upc.entity.Empresavuelo;
 import pe.edu.upc.service.IEmpresaEstadiaService;
 import pe.edu.upc.service.IEmpresaVueloService;
 
@@ -11,10 +20,38 @@ import pe.edu.upc.service.IEmpresaVueloService;
 public class EmpresasController {
 	
 	@Autowired
-	private IEmpresaVueloService serviciovuelo;
+	private IEmpresaVueloService servicioVuelo;
 	
 	@Autowired
-	private IEmpresaEstadiaService servicioestadial;
+	private IEmpresaEstadiaService servicioEstadia;
 	
-	//aqui van las funciones que dependen de las vistas.
+	
+	
+	
+	
+	//TODO: EMPRESA VUELO
+	@GetMapping(value="/empresavuelo/registrar")
+	public String registrarEV(Model model){
+		model.addAttribute("titulo","Registrar Aerolinea");
+		model.addAttribute("empresavuelo", new Empresavuelo());
+		return "empresavuelo/registrar";
+	}
+	@RequestMapping(value = "/empresavuelo/registrar", method= RequestMethod.POST)
+	public String guardarEmpresaVuelo(@Valid Empresavuelo empresavuelo, BindingResult bindingResult) {
+		servicioVuelo.saveEmpresavuelo(empresavuelo);
+		return "redirect:/";
+	}
+	
+	//TODO: EMPRESA ESTADIA
+	@GetMapping(value="/empresaestadia/registrar")
+	public String registrarES(Model model){
+		model.addAttribute("titulo","Registrar Empresa de Estadias");
+		model.addAttribute("empresaestadia", new Empresaestadia());
+		return "empresaestadia/registrar";
+	}
+	@RequestMapping(value = "/empresaestadia/registrar", method= RequestMethod.POST)
+	public String guardarEmpresaEstadia(@Valid Empresaestadia empresaestadia, BindingResult bindingResult) {
+		servicioEstadia.saveEmpresaestadia(empresaestadia);
+		return "redirect:/";
+	}
 }
--- src/main/java/pe/edu/upc/controller/UsuarioController.java
+++ src/main/java/pe/edu/upc/controller/UsuarioController.java
@@ -1,9 +1,17 @@
 package pe.edu.upc.controller;
 
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import pe.edu.upc.entity.Usuario;
 import pe.edu.upc.service.IUsuarioService;
 
 @Controller
@@ -12,6 +20,17 @@ public class UsuarioController {
 	@Autowired
 	private IUsuarioService servicio;
 	
-	//aqui van las funciones que dependen de las vistas.
 	
+	@GetMapping(value="/usuario/registrar")
+	public String registrar(Model model){
+		model.addAttribute("titulo","Registrar usuario");
+		model.addAttribute("usuario", new Usuario());
+		return "usuario/registrar";
+	}
+	
+	@RequestMapping(value = "/usuario/registrar", method= RequestMethod.POST)
+	public String guardar(@Valid Usuario usuario, BindingResult bindingResult) {
+		servicio.saveUsuario(usuario);
+		return "redirect:/";
+	}
 }
