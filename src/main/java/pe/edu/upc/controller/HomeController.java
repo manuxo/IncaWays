66363ed0456511beb65c09612f803d2047284823
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-05-27 22:35:38 -0500

    Set title to index.html and ver.html fixed
    Se realizaron cambios en el controlador y en la vista index, se agrego
    el nombre de la empresa en la vista ver.html
    
    Hito 1 completo: Create and read entities from database.

--- src/main/java/pe/edu/upc/controller/HomeController.java
+++ src/main/java/pe/edu/upc/controller/HomeController.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value= "/")
 public class HomeController {
 	@GetMapping("/")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("titulo","INCAWAYS - Home");
 		return "index";
 	}
 }
